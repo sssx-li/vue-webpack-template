9ac48ad08fc1e9d4ba3d7b3ff327e97a520ff72f
lizhiquan <842048365@qq.com>
2023-06-02 09:48:54 +0800

    perf: 移动mock包;优化filters

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,23 +1,8 @@
 import type { App } from 'vue';
-import { useDateFormat } from '@vueuse/core';
-import type { MaybeComputedRef, DateLike, UseDateFormatOptions } from '@vueuse/core';
-
-export interface IFilters {
-  dateFormat: (
-    date: MaybeComputedRef<DateLike>,
-    formatStr?: MaybeComputedRef<string>,
-    options?: UseDateFormatOptions
-  ) => string;
-}
+import { dateFormat } from '@/utils';
 
 export default function filters(app: App) {
   app.config.globalProperties.$filters = {
-    dateFormat(
-      date: MaybeComputedRef<DateLike>,
-      formatStr: MaybeComputedRef<string> = 'YYYY-MM-DD hh:mm:ss',
-      options?: UseDateFormatOptions
-    ) {
-      return useDateFormat(date, formatStr, options).value;
-    },
+    dateFormat,
   };
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './objectToSearch';
+export * from './dateFormat';
